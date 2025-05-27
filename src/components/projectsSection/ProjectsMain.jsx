3a248fb2ec5cf424fbc46367cd673f7d736c35d5
kunlexinc <mkunle6@gmail.com>
2025-05-27 10:27:59 +0100

    COM5 - fifth commit

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -61,6 +61,7 @@ const ProjectsMain = () => {
               year={project.year}
               align={project.align}
               image={project.image}
+              link={project.link}
             />
           );
         })}
