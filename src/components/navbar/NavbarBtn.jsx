7ffa9b05c30c7a3ec6ca5db01d1871348638006c
kunlexinc <mkunle6@gmail.com>
2025-05-27 02:41:33 +0100

    COM3 - third commit

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -6,6 +6,7 @@ const NavbarBtn = () => {
     <>
        <a
   href="https://drive.google.com/file/d/1HyGSfO-3N_MY3yzIrqbqBNIdyHJygzk9/view?usp=sharing"
+   target="_blank"
  
  className=" px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow"
 >
